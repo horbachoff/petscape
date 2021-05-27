0ce2f2d83a8261e3193ce180e9bd977b173644c7
yehor horbachov <ien.gorov@gmail.com>
2021-05-27 16:23:50 -0500

    Devise implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  devise_for :users
   get '/nanny_dashboard/', to: 'nanny_dashboard#nanny_dashboard'
 
   get '/dashboard', to: 'dashboard#dashboard'
