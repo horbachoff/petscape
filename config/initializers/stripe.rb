cb2925068bfd897733a17d441560be1b50c8e7cd
yehor horbachov <ien.gorov@gmail.com>
2021-06-07 13:17:40 -0500

    conflict

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,11 +4,7 @@ Rails.configuration.stripe = {
     secret_key:      ENV['STRIPE_SECRET_KEY'],
     signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
   }
-  
+
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
-
-# StripeEvent.configure do |events|
-#   events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
-# end
