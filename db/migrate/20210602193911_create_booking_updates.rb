584054e60f6d991deedb4d9d64d9c300d6325ae2
yehor horbachov <ien.gorov@gmail.com>
2021-06-11 15:45:56 -0500

    fix(db): Deleted migration

--- db/migrate/20210602193911_create_booking_updates.rb
+++ db/migrate/20210602193911_create_booking_updates.rb
@@ -3,6 +3,7 @@ class CreateBookingUpdates < ActiveRecord::Migration[6.0]
     create_table :booking_updates do |t|
       t.text :content
       t.references :booking, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210611150732_add_user_reference_to_booking_updates.rb
+++
@@ -1,5 +0,0 @@
-class AddUserReferenceToBookingUpdates < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :booking_updates, :user, null: false, foreign_key: true
-  end
-end
